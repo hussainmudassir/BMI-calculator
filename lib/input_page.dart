27ba933f2e48ebfecf9958b546932d1400dd375c
hussainmudassir <mh_487@yahoo.co.in>
2023-01-16 20:39:30 -0500

    Added icon selections and GestureDetector

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const Color activeCardColor = Color(0xFF1D1E33);
+const Color inactiveCardColor = Color(0xFF111328);
+
+enum Gender { MALE, FEMALE }
 
 class InputPage extends StatefulWidget {
   @override
@@ -6,6 +12,8 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,32 +25,95 @@ class _InputPageState extends State<InputPage> {
           Expanded(
               child: Row(
             children: <Widget>[
-              Expanded(child: ReusableCard(clr: Color(0xFF1D1E33))),
-              Expanded(child: ReusableCard(clr: Color(0xFF1D1E33)))
+              Expanded(
+                child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.MALE;
+                      });
+                    },
+                    child: ReusableCard(
+                      clr: selectedGender == Gender.MALE
+                          ? activeCardColor
+                          : inactiveCardColor,
+                      cardChild: GenderCard(
+                          gender: Gender.MALE.name,
+                          icon: FontAwesomeIcons.mars),
+                    )),
+              ),
+              Expanded(
+                  child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedGender = Gender.FEMALE;
+                        });
+                      },
+                      child: ReusableCard(
+                        clr: selectedGender == Gender.FEMALE
+                            ? activeCardColor
+                            : inactiveCardColor,
+                        cardChild: GenderCard(
+                            gender: Gender.FEMALE.name,
+                            icon: FontAwesomeIcons.venus),
+                      )))
             ],
           )),
-          Expanded(child: ReusableCard(clr: Color(0xFF1D1E33))),
+          Expanded(child: ReusableCard(clr: activeCardColor)),
           Expanded(
               child: Row(
             children: <Widget>[
-              Expanded(child: ReusableCard(clr: Color(0xFF1D1E33))),
-              Expanded(child: ReusableCard(clr: Color(0xFF1D1E33)))
+              Expanded(child: ReusableCard(clr: activeCardColor)),
+              Expanded(child: ReusableCard(clr: activeCardColor))
             ],
-          ))
+          )),
+          // Expanded(
+          Container(
+            color: Colors.red,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: 80,
+          )
         ],
       ),
     );
   }
 }
 
+class GenderCard extends StatelessWidget {
+  GenderCard({@required this.gender, @required this.icon});
+  final String gender;
+  final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          gender,
+          style: TextStyle(fontSize: 18, color: Color(0xFF8D8E98)),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.clr});
+  ReusableCard({@required this.clr, this.cardChild});
   final Color clr;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15),
+      child: cardChild,
       decoration:
           BoxDecoration(color: clr, borderRadius: BorderRadius.circular(10)),
     );
